98008738577a02e3a49afff4183d0967bb9d63bd
renansousasilva1 <84554881+renansousasilva1@users.noreply.github.com>
2025-02-27 22:34:24 -0300

    Delete clima directory

--- clima/clima/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cidade
-
-@admin.register(Cidade)
-class CidadeAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'codigo_climatempo', 'link_precipitacao')
-    search_fields = ('nome',)
